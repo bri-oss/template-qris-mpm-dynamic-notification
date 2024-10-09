6f7d9cf351b9c6d0c181bfeddbbc2efa75f526bf
AlfaRiza <malfariza45@gmail.com>
2024-10-09 13:17:27 +0700

    update: add variable

--- src/get_token.php
+++ src/get_token.php
@@ -5,8 +5,8 @@ use BRI\Util\GetAccessToken;
 require __DIR__ . '/../../briapi-sdk/autoload.php';
 
 // env values
-$clientId = '';
-$clientSecret = '';
+$clientId = 'your_client_id';
+$clientSecret = 'super_secret';
 
 // url path values
 $baseUrl = 'https://api.bridex.qore.page/mock'; //base url
--- src/notify_payment.php
+++ src/notify_payment.php
@@ -5,19 +5,19 @@ use BRI\QrisMPMDynamicNotification\NotifyPayment;
 require __DIR__ . '/../../briapi-sdk/autoload.php';
 
 // env values
-$clientId = '';
-$clientSecret = '';
+$clientId = 'your_client_id';
+$clientSecret = 'super_secret';
 
 // url path values
 $baseUrl = 'https://api.bridex.qore.page/mock'; //base url
 
-$externalId = '';
-$origin = '';
-$ipAddress = '';
-$deviceId = '';
-$latitude = '';
-$longitude = '';
-$channelId = '';
+$externalId = '41807553358950093184162180797837';
+$origin = 'bri.co.id';
+$ipAddress = '172.24.281.24';
+$deviceId = '09864ADCASA';
+$latitude = '-6.108841';
+$longitude = '106.7782137';
+$channelId = '95221';
 
 $qrisMpmDynamicNotification = new NotifyPayment();
 
