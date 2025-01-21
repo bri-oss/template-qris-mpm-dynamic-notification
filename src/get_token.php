64d2d5dbaebd20b526d7af6dcf45d260a08a2345
AlfaRiza <malfariza45@gmail.com>
2025-01-21 14:41:36 +0700

    feat: xss attact and try catch declaration

--- src/get_token.php
+++ src/get_token.php
@@ -2,6 +2,9 @@
 
 require 'utils.php';
 
+header('Strict-Transport-Security: max-age=31536000; includeSubDomains; preload');
+header('Content-Type: application/json'); // Ensure all responses are JSON
+
 // url path values
 $baseUrl = 'https://api.bridex.qore.page/mock'; //base url
 
@@ -14,8 +17,23 @@ try {
     $privateKey
   );
 
-  echo $accessToken;
+  echo htmlspecialchars($accessToken, ENT_QUOTES, 'UTF-8');
+} catch (InvalidArgumentException $e) {
+  // Return a generic error message to the client
+  http_response_code(400); // Bad Request
+
+  // Log the error for debugging
+  error_log('InvalidArgumentException: ' . $e->getMessage());
+} catch (RuntimeException $e) {
+  // Return a generic error message to the client
+  http_response_code(500); // Internal Server Error
+
+  // Log the error for debugging
+  error_log('RuntimeException: ' . $e->getMessage());
 } catch (Exception $e) {
-  error_log('Error: ' . $e->getMessage());
-  exit(1);
+  // Return a generic error message to the client
+  http_response_code(500); // Internal Server Error
+
+  // Log any other unexpected errors for debugging
+  error_log('UnexpectedException: ' . $e->getMessage());
 }
